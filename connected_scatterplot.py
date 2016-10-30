8691c129e004bb399f7883018dc9d6664f768fe4
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-10-30 21:33:17 +0900

    Update: add csv reader

--- connected_scatterplot.py
+++ connected_scatterplot.py
@@ -4,4 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-f = open('../csv/test.csv', 'w')
+f = open('./csv/kyoto_kisyotyo.csv', 'r')
+reader = csv.reader(f)
+header = next(reader)
+for row in reader:
+    print row
